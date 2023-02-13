4919b9e0d1b42186d51141cba64082227fd1d5ed
Adson Gomes Olivera <adsonprofessionalemail@gmail.com>
2023-02-13 15:14:09 -0300

    <setup> Setting up order service

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const routes = require('./routes');
+const errorMiddleware = require('./middlewares/error.middleware');
 const swaggerUI = require('swagger-ui-express');
 const swaggerDocument = require('../swagger/accounts-swagger.json');
 
@@ -15,6 +16,7 @@ app.use(helmet());
 app.get('/health-test', (_req, res) => res.status(200).send('Connection OK'));
 app.use('/api/users', routes.usersRoutes);
 app.use('/api/admin/users', routes.usersAdminRoutes);
+app.use(errorMiddleware);
 
 app.use('/api-docs', swaggerUI.serve);
 app.get('/api-docs', swaggerUI.setup(swaggerDocument));
