cdb345a96437965b1e2050c32ec440a000aac5b8
Adson Gomes Olivera <adsonprofessionalemail@gmail.com>
2023-03-08 14:47:29 -0300

    <feat> Created JWT functions

---
+++ src/helpers/token.jwt.js
@@ -0,0 +1,23 @@
+const JWT = require('jsonwebtoken');
+const CustomError = require('./error.custom');
+const HTTPStatus = require('./HTTP.status');
+require('dotenv');
+
+const generateToken = (jwtPayload) => {
+  const token = JWT.sign(jwtPayload, process.env.JWT_SECRET, {
+    algorithm: 'HS256',
+    expiresIn: '1h',
+  });
+
+  return token;
+};
+
+const verifyToken = (token) => {
+  const verify = JWT.verify(token, process.env.JWT_SECRET);
+  if (!verify) throw new CustomError('Token is invalid !', HTTPStatus.UNAUTHORIZED);
+};
+
+module.exports = {
+  generateToken,
+  verifyToken,
+};
