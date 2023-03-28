b3824525cbacd13e5ef81b5eb862245300435192
Adson Gomes Olivera <adsonprofessionalemail@gmail.com>
2023-03-28 19:00:11 -0300

    <feat> API Gateway done

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM node:18-alpine
 WORKDIR /app/account
 COPY package.json ./
-# COPY .npmrc ./
 RUN npm install
 COPY . ./
 ENTRYPOINT npm start
--- src/routes/accounts.routes.js
+++ src/routes/accounts.routes.js
@@ -3,9 +3,9 @@ const accountsController = require('../controllers/accounts.controller');
 
 const router = Router();
 
-router.post('/', accountsController.create);
 router.get('/', accountsController.findAll);
 router.get('/:id', accountsController.findOne);
+router.post('/', accountsController.create);
 router.post('/byEmail', accountsController.findOneByEmail);
 router.put('/:id', accountsController.update);
 router.delete('/:id', accountsController.deleteOne);
